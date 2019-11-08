ec3a4c4b45444fe3b7436ec529aa9eb2fde64a99
Santiago Mondragón <santimondragonm>
2019-11-08 18:14:53 -0500

    working on Jefe de Departamento

--- js/login.js
+++ js/login.js
@@ -3,15 +3,15 @@ let login = document.getElementById("loginBtn");
 
 login.addEventListener("click", (event) => {
     if(type.value === "profe") {
-        window.location.href = "/profesor/home.html";
+        window.location.href = "./profesor/home.html";
     } else if(type.value === "dpto") {
-        window.location.href = "/departamento/home.html";
+        window.location.href = "./departamento/home.html";
     } else if(type.value === "tech") {
-        window.location.href = "/tecnologia/home.html";
+        window.location.href = "./tecnologia/home.html";
     } else if(type.value === "compras") {
-        window.location.href = "/compras/home.html";
+        window.location.href = "./compras/home.html";
     } else if(type.value === "soporte") {
-        window.location.href = "/soporte/home.html";
+        window.location.href = "./soporte/home.html";
     } else {
         alert("Debes escoger un rol");
     }
